04db08f5cc5593580c81be21ae3241ca4d2dfea7
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-03-29 05:38:34 +0600

    responsive - for md and sm

--- client/src/App.js
+++ client/src/App.js
@@ -7,54 +7,57 @@ import "./index.css";
 
 function App() {
   return (
-    <div className="p-20 bg-slate-200">
+    <div className="lg:p-20 sm:p-5 md:p-5 bg-slate-200">
       <div className="bg-white">
         {/* navbar */}
-        <div className="flex justify-between items-center py-10 px-5">
+        <div className="flex justify-between items-center py-4 px-4 sm:py-10 sm:px-10">
           <h1 className="font-bold text-2xl">Reservation</h1>
           <button className="border rounded-md bg-blue-500 py-2 px-2 text-white font-md hover:bg-black">
             Print / Download
           </button>
         </div>
         {/* Form Section */}
-        <div className="flex">
+        <div className="flex flex-col md:flex-row ">
+          {/* Left Section */}
           <div className="flex-grow">
-            <div className="grid px-5 gap-6">
-              <h2 className="font-bold border-b-2 border-indigo-200 pb-1">
+            <div className="grid gap-6 px-5">
+              <h2 className="font-bold border-b-2 border-indigo-200 pb-2">
                 Reservation Details
               </h2>
-              <div className="grid gap-5 rounded-md border-solid border-2 border-indigo-200 p-5">
+              <div className="grid gap-4 rounded-md border-solid border-2 border-indigo-200 p-4">
                 <ReservationDetailsForm />
               </div>
-              <h2 className="font-bold border-b-2 border-indigo-200 pb-1">
+              <h2 className="font-bold border-b-2 border-indigo-200 pb-2">
                 Vehicle Information
               </h2>
-              <div className="grid gap-5 rounded-md border-solid border-2 border-indigo-200 p-5 mb-10">
+              <div className="grid gap-4 rounded-md border-solid border-2 border-indigo-200 p-4">
                 <VehicleInformation />
               </div>
             </div>
           </div>
-          <div className="flex-grow">
-            <div className="grid gap-6">
-              <h2 className="font-bold border-b-2 border-indigo-200 pb-1">
+          {/* Middle Section */}
+          <div className="flex-grow mt-6 md:mt-0">
+            <div className="grid gap-6 px-2">
+              <h2 className="font-bold border-b-2 border-indigo-200 pb-2">
                 Customer Information
               </h2>
-              <div className="grid gap-5 rounded-md border-solid border-2 border-indigo-200 p-5">
+              <div className="grid gap-4 rounded-md border-solid border-2 border-indigo-200 p-4">
                 <CustomerDetails />
               </div>
-              <h2 className="font-bold border-b-2 border-indigo-200 pb-1">
+              <h2 className="font-bold border-b-2 border-indigo-200 pb-2">
                 Additional Charges
               </h2>
-              <div className="grid gap-5 rounded-md border-solid border-2 border-indigo-200 p-5">
+              <div className="grid gap-4 rounded-md border-solid border-2 border-indigo-200 p-4">
                 <AdditionalCharges />
               </div>
             </div>
           </div>
-          <div className="flex-grow ml-5">
-            <h2 className="font-bold border-b-2 border-indigo-200 pb-1">
+          {/* Right Section */}
+          <div className="flex-grow mt-6 md:mt-0 px-2">
+            <h2 className="font-bold border-b-2 border-indigo-200 pb-2">
               Charges Summary
             </h2>
-            <div className=" rounded-md border-solid border-2 border-indigo-200 p-5 mt-6">
+            <div className="rounded-md border-solid border-2 border-indigo-200 p-4 mt-4">
               <ChargesSummary />
             </div>
           </div>
